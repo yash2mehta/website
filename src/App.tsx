c104b99990834e81efc1f5c5bc1369da0e07dc8a
jh1h1h <cjh.t01snake@gmail.com>
2023-04-15 08:08:13 +0800

    Revert "split header into new component"
    This reverts commit 1285355a81b27335e4114441aea9932839a98f61.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,64 @@
 import { useState } from 'react'
 import logo from './assets/logo.svg'
 import './App.css'
-import Header from './components/layout/header/Header'
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
+      <div className="header">
+        <a href="https://market.watch" target="_blank">
+          <img src={logo} className="logo" alt="MarketWatch logo" />
+        </a>
+        <div className="navbar">
+          <div className="dropdown">
+            <button className="dropbtn">About</button>
+            <div className="dropdown-content">
+              <a href="#">Mission and Vision</a>
+              <a href="#">Club Structure</a>
+              <a href="#">Management</a>
+              <a href="#">Testimonials</a>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Blog</button>
+            {/* <div className="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div> */}
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Student Community</button>
+            <div className="dropdown-content">
+              <a href="#">Research Publications</a>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Events</button>
+            <div className="dropdown-content">
+              <a href="#">2022</a>
+              <a href="#">2023</a>
+              <a href="#">Gallery</a>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Industry Collaborations</button>
+            <div className="dropdown-content">
+              <a href="#">Sponsors</a>
+              <a href="#">Partnerships</a>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Contact Us</button>
+            {/* <div className="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div> */}
+          </div>
+        </div>
+      </div>
       <div className="body">
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod quis ligula sit amet tempor. Nullam nibh ipsum, malesuada sed urna sed, feugiat suscipit odio. Nunc ac sem nisi. Aenean at ornare purus, et condimentum orci. Vivamus sodales molestie elit, et rutrum sapien pharetra id. Etiam tincidunt mi gravida suscipit fringilla. Cras vestibulum ipsum in tortor vestibulum vestibulum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sodales lobortis ligula a dapibus. Sed a mauris vel mauris iaculis cursus in convallis magna. Praesent congue orci nisi, vestibulum laoreet nisi maximus nec. Morbi convallis eu risus vel egestas.
