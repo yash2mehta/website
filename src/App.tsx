33e2c4fbcc17649880a5bcd1dabe27efacdd26e3
jh1h1h <cjh.t01snake@gmail.com>
2023-04-15 08:09:32 +0800

    Revert "header v1, pending width responsive sizing, and more"
    This reverts commit f0fb8f946d8f51f517f68476bb479881e17d9249.

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,33 @@
 import { useState } from 'react'
-import logo from './assets/logo.svg'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div className="header">
-        <a href="https://market.watch" target="_blank">
-          <img src={logo} className="logo" alt="MarketWatch logo" />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <div className="navbar">
-          <div className="dropdown">
-            <button className="dropbtn">About</button>
-            <div className="dropdown-content">
-              <a href="#">Mission and Vision</a>
-              <a href="#">Club Structure</a>
-              <a href="#">Management</a>
-              <a href="#">Testimonials</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Blog</button>
-            {/* <div className="dropdown-content">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
-            </div> */}
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Student Community</button>
-            <div className="dropdown-content">
-              <a href="#">Research Publications</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Events</button>
-            <div className="dropdown-content">
-              <a href="#">2022</a>
-              <a href="#">2023</a>
-              <a href="#">Gallery</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Industry Collaborations</button>
-            <div className="dropdown-content">
-              <a href="#">Sponsors</a>
-              <a href="#">Partnerships</a>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">Contact Us</button>
-            {/* <div className="dropdown-content">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
-            </div> */}
-          </div>
-        </div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <div className="body">
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod quis ligula sit amet tempor. Nullam nibh ipsum, malesuada sed urna sed, feugiat suscipit odio. Nunc ac sem nisi. Aenean at ornare purus, et condimentum orci. Vivamus sodales molestie elit, et rutrum sapien pharetra id. Etiam tincidunt mi gravida suscipit fringilla. Cras vestibulum ipsum in tortor vestibulum vestibulum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sodales lobortis ligula a dapibus. Sed a mauris vel mauris iaculis cursus in convallis magna. Praesent congue orci nisi, vestibulum laoreet nisi maximus nec. Morbi convallis eu risus vel egestas.
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   )
 }
