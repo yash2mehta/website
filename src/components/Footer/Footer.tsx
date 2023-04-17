c81592e0b09fc88007c57d396836451b976cba5b
jh1h1h <cjh.t01snake@gmail.com>
2023-04-17 13:25:52 +0800

    footer mobile design done, pending links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,33 @@
 import './Footer.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faFacebook } from '@fortawesome/free-regular-svg-icons'
 
 function Footer() {
   
     return (
-      <div className="footer"></div>
+      <div className="footer">
+        <div className="centered-container">
+          <div className="icon-container">
+            <a href="#">
+              <FaInstagram/>
+            </a>
+            <a href="#">
+              <FaFacebook/>
+            </a>
+            <a href="#">
+              <FaLinkedin/>
+            </a>
+            <a href="#">
+              <FiMail/>
+            </a>
+          </div>  
+          <div className="copyright">
+            © COPYRIGHT - SUTD MARKETWATCH CLUB 2023
+          </div>
+        </div>        
+      </div>
     )
   }
   
