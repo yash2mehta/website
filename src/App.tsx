7fe87407b625af3f989ea057207dc9a3f38e7079
Daniel Koh <aikhong21@gmail.com>
2023-04-19 00:52:14 +0800

    integrated the contactus page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar2 from './Navbar/Navbar2'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from "react";
+import "./App.css";
+import Navbar2 from "./components/Navbar/Navbar2";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import ClubStructure from './AboutUs/ClubStructure';
-import ClubAdvisors from './AboutUs/ClubAdvisors';
-import Testimonials from './AboutUs/Testimonials';
-import ManagementCommittee from './AboutUs/ManagementCommittee';
+import ClubStructure from "./AboutUs/ClubStructure";
+import ClubAdvisors from "./AboutUs/ClubAdvisors";
+import Testimonials from "./AboutUs/Testimonials";
+import ManagementCommittee from "./AboutUs/ManagementCommittee";
+import ContactUs from "./ContactUs/ContactUs";
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Navbar2/>
-        <Routes>
-          <Route path='/aboutus/clubstructure' Component={ClubStructure} />
-          <Route path='/aboutus/clubadvisors' Component={ClubAdvisors} />
-          <Route path='/aboutus/testimonials' Component={Testimonials} />
-          <Route path='/aboutus/managementcommittee' Component={ManagementCommittee} />
-        </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Navbar2 />
+			<Routes>
+				<Route path="/aboutus/clubstructure" Component={ClubStructure} />
+				<Route path="/aboutus/clubadvisors" Component={ClubAdvisors} />
+				<Route path="/aboutus/testimonials" Component={Testimonials} />
+				<Route
+					path="/aboutus/managementcommittee"
+					Component={ManagementCommittee}
+				/>
+				<Route path="/contactus" Component={ContactUs} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
