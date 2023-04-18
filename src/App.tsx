c2c8cb1c366c49007310170f2c7232cdf481c08f
jh1h1h <76950331+jh1h1h@users.noreply.github.com>
2023-04-18 14:04:00 +0800

    Revert "Navbar styling and tailwind css config file"

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,18 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-
 import Navbar from './Navbar/Navbar';
-
-import Navbar2 from './Navbar/Navbar2'
-
-import { BrowserRouter,Routes, Route } from 'react-router-dom'
-
-import ClubStructure from './AboutUs/ClubStructure';
-import ClubAdvisors from './AboutUs/ClubAdvisors';
-import Testimonials from './AboutUs/Testimonials';
-import ManagementCommittee from './AboutUs/ManagementCommittee';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
-        <Navbar2/>
-        <Routes>
-          <Route path='/aboutus/clubstructure' Component={ClubStructure} />
-          <Route path='/aboutus/clubadvisors' Component={ClubAdvisors} />
-          <Route path='/aboutus/testimonials' Component={Testimonials} />
-          <Route path='/aboutus/managementcommittee' Component={ManagementCommittee} />
-        </Routes>
+      <div className="App">
+        <Navbar/>
+        <h1>Some content</h1>
+      </div>
     </BrowserRouter>
   )
 }
